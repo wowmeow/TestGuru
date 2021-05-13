904bb93eab3b044b5d2508dc0ec582652a693fe5
wowmeow <anna.shigirdanova@yandex.ru>
2021-05-13 16:03:21 +0300

    update

--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,11 @@ ActiveRecord::Schema.define(version: 2021_05_07_193038) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
-    t.string "author", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_05_07_193038) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create!([{ name: 'Иван Иванов', role: 'user', login: 'ivan1', password: 'ivan1' },
+                      { name: 'Софья Иванова', role: 'user', login: 'sonya', password: 'sonya1' },
+                      { name: 'Степан Петров', role: 'user', login: 'stepan', password: 'stepan1' }])
+
+categories = Category.create!([{ name: 'Программирование' }])
+
+tests = Test.create!([{ title: 'RoR', level: 0, category: categories[0], user: users[0] },
+                      { title: 'RoR', level: 1, category: categories[0], user: users[1] },
+                      { title: 'C++', level: 0, category: categories[0], user: users[1] },
+                      { title: 'C#', level: 0, category: categories[0], user: users[2] }])
+
+questions = Question.create!([{ text: 'Какой-то вопрос для теста по RoR уровня 0', test_id: 1 },
+                              { text: 'Какой-то вопрос для теста по RoR уровня 1', test_id: 2 },
+                              { text: 'Какой-то текст для теста по RoR уровня 0', test_id: 1 },
+                              { text: 'Какой-то текст для теста по С++ уровня 0', test_id: 3 },
+                              { text: 'Какой-то текст для теста по С# уровня 0', test_id: 4 }])
+
+answers = Answer.create!([{ title: 'yes', correct: true, question: questions[0] },
+                          { title: 'no', correct: false, question: questions[0] },
+                          { title: 'yes', correct: true, question: questions[1] },
+                          { title: 'no', correct: false , question: questions[1] },
+                          { title: 'class', correct: true, question: questions[2] },
+                          { title: 'method', correct: false, question: questions[2] },
+                          { title: 'yes', correct: false, question: questions[3] },
+                          { title: 'no', correct: true, question: questions[3] },
+                          { title: '1', correct: true, question: questions[4] },
+                          { title: '2', correct: false, question: questions[4] },
+                          { title: '3', correct: false, question: questions[4] }])
