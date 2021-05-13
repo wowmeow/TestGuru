a7107bfa36733675c05fec3f79de2dc296003c6c
wowmeow <anna.shigirdanova@yandex.ru>
2021-05-13 17:32:10 +0300

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,12 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+
+  def self.with_category(category_name)
+    joins(:category)
+      .where(categories: { title: category_name })
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :tests
+  has_many :results, dependent: :destroy
+
   def current_tests_by(level)
     tests.where(level: level)
   end
