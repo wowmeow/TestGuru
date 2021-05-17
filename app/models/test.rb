92bcaa903d384e69936eb42d59f0fd034644693c
wowmeow <anna.shigirdanova@yandex.ru>
2021-05-17 21:43:50 +0300

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
+  has_many :results
+  has_many :users, through: :results
+
   def self.with_category(category_name)
     joins('JOIN categories ON tests.category_id == categories.id')
       .where(categories: { title: category_name })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # belongs_to :category
+  # has_many :tests
+  # has_many :questions
+  # has_many :results
+  # has_many :tests, through: :results
+
   def all_tests_with(level)
     Test.joins('JOIN results ON tests.id == results.test_id')
         .where(results: { user_id: id })
