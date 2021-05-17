40ce7862625a55c141ae20f65af7d35ecbe37b6c
wowmeow <anna.shigirdanova@yandex.ru>
2021-05-17 21:38:29 +0300

    add 'progress' column in results table

--- db/migrate/20210513135331_create_results.rb
+++ db/migrate/20210513135331_create_results.rb
@@ -3,6 +3,7 @@ class CreateResults < ActiveRecord::Migration[6.1]
     create_table :results do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
+      t.integer :progress, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_135331) do
   create_table "results", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
+    t.integer "progress", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_results_on_test_id"
