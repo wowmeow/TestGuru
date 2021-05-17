14ff1d170217e7823dfd6d6afc682403c586f262
wowmeow <anna.shigirdanova@yandex.ru>
2021-05-17 23:48:06 +0300

    delete 'id' ending at all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Answer.delete_all
 Question.delete_all
 Test.delete_all
@@ -16,32 +8,33 @@ users = User.create!([{ name: 'Иван Иванов', role: 'user', login: 'iva
                       { name: 'Софья Иванова', role: 'user', login: 'sonya', password: 'sonya1' },
                       { name: 'Степан Петров', role: 'user', login: 'stepan', password: 'stepan1' }])
 
+
 categories = Category.create!([{ title: 'Программирование' },
                                { title: 'Дизайн' }])
 
-tests = Test.create!([{ title: 'RoR', level: 0, category_id: categories[0].id, author_id: users[0].id },
-                      { title: 'RoR', level: 1, category_id: categories[0].id, author_id: users[1].id },
-                      { title: 'C++', level: 0, category_id: categories[0].id, author_id: users[1].id },
-                      { title: 'C#', level: 0, category_id: categories[0].id, author_id: users[2].id }])
+tests = Test.create!([{ title: 'RoR', level: 0, category: categories[0], author: users[0] },
+                      { title: 'RoR', level: 1, category: categories[0], author: users[1] },
+                      { title: 'C++', level: 0, category: categories[0], author: users[1] },
+                      { title: 'C#', level: 0, category: categories[0], author: users[2] }])
 
-questions = Question.create!([{ text: 'Rails - это ...', test_id: tests[0].id },
-                              { text: 'Ruby относится к высокоуровнему языку программирования?', test_id: tests[1].id },
-                              { text: 'Rails реализует архитектурный шаблон Model-View-Controller?', test_id: tests[1].id },
-                              { text: 'В каком году возник С++?', test_id: tests[2].id },
-                              { text: 'В какой компании работали созатели С#?', test_id: tests[3].id }])
+questions = Question.create!([{ text: 'Rails - это ...', test: tests[0] },
+                              { text: 'Ruby относится к высокоуровнему языку программирования?', test: tests[1] },
+                              { text: 'Rails реализует архитектурный шаблон Model-View-Controller?', test: tests[1] },
+                              { text: 'В каком году возник С++?', test: tests[2] },
+                              { text: 'В какой компании работали созатели С#?', test: tests[3] }])
 
-answers = Answer.create!([{ title: 'язык программирования', correct: false, question_id: questions[0].id },
-                          { title: 'фреймворк', correct: true, question_id: questions[0].id },
-                          { title: 'да', correct: true, question_id: questions[1].id },
-                          { title: 'нет', correct: false, question_id: questions[1].id },
-                          { title: 'да', correct: true, question_id: questions[2].id },
-                          { title: 'нет', correct: false, question_id: questions[2].id },
-                          { title: '1980', correct: true, question_id: questions[3].id },
-                          { title: '1981', correct: false, question_id: questions[3].id },
-                          { title: '1990', correct: false, question_id: questions[3].id },
-                          { title: 'Google', correct: false, question_id: questions[4].id },
-                          { title: 'Microsoft', correct: true, question_id: questions[4].id }])
+answers = Answer.create!([{ title: 'язык программирования', correct: false, question: questions[0] },
+                          { title: 'фреймворк', correct: true, question: questions[0] },
+                          { title: 'да', correct: true, question: questions[1] },
+                          { title: 'нет', correct: false, question: questions[1] },
+                          { title: 'да', correct: true, question: questions[2] },
+                          { title: 'нет', correct: false, question: questions[2] },
+                          { title: '1980', correct: true, question: questions[3] },
+                          { title: '1981', correct: false, question: questions[3] },
+                          { title: '1990', correct: false, question: questions[3] },
+                          { title: 'Google', correct: false, question: questions[4] },
+                          { title: 'Microsoft', correct: true, question: questions[4] }])
 
-result = Result.create!([{ user_id: users[0].id, test_id: tests[0].id },
-                         { user_id: users[0].id, test_id: tests[1].id },
-                         { user_id: users[0].id, test_id: tests[2].id }])
+result = Result.create!([{ user: users[0], test: tests[0] },
+                         { user: users[0], test: tests[1] },
+                         { user: users[0], test: tests[2] }])
