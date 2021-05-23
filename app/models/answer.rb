bf01d5e6a431d6acdd065e87244298d2269b3db3
wowmeow <anna.shigirdanova@yandex.ru>
2021-05-23 20:34:02 +0300

    add scope

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :correct_answers, -> { where(correct: true) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,10 @@ class Test < ApplicationRecord
   has_many :users, through: :results, dependent: :destroy
   has_many :questions
 
+  scope :easy_level, -> { where(level: (0..1)) }
+  scope :medium_level, -> { where(level: (2..4)) }
+  scope :hard_level, -> { where(level: (5..Float::INFINITY)) }
+
   def self.with_category(category_name)
     joins(:category)
       .where(categories: { title: category_name })
