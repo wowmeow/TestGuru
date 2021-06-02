30cbfd62ad97beba7ca2890bb48764c89dd240ed
wowmeow <anna.shigirdanova@yandex.ru>
2021-06-03 00:56:31 +0300

    add methods and fix html

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,39 @@
 class QuestionsController < ApplicationController
+  before_action :find_test
+
+  def index
+    render plain: @test.questions.pluck(:text)
+  end
+
+  def create
+    question = @test.questions.new(test_params)
+    if question.save
+      redirect_to question
+    else
+      render :new
+    end
+  end
+
+  def new; end
+
+  def edit; end
+
+  def show
+    render plain: @question.title
+  end
+
+  def update; end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def test_params
+    params.require(:question).permit(:text)
+  end
 end
