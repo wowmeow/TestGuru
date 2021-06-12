29c558b3cfff363991590daa1540ba847491acca
wowmeow <anna.shigirdanova@yandex.ru>
2021-06-12 16:48:53 +0300

    add view of all tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,10 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show]
 
+  def index
+    @tests = Test.all
+  end
+
   def show
     @test = find_test
   end
@@ -9,5 +13,9 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :category_id, :author_id)
   end
 end
