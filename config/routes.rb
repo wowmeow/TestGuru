cbd755448c90afd2413dcdbbb78903dc5bd2c07f
wowmeow <anna.shigirdanova@yandex.ru>
2021-06-13 18:41:52 +0300

    add all for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 end
