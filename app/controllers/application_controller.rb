f3922db414be5a484156a73446b8104be8fbda9d
wowmeow <anna.shigirdanova@yandex.ru>
2021-06-16 18:09:45 +0300

    add flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user,
+                :logged_in?
 
   private
 
   def authenticate_user!
-    redirect_to login_path unless current_user
+    redirect_to login_path, alert: 'Неверный логин или пароль' unless current_user
   end
 
   def current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(login: params[:login])
@@ -9,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Неверный логин или пароль'
       render :new
     end
   end
