bb4a7aa1ff4ccc30652a77345a0ea9701715f6e1
wowmeow <anna.shigirdanova@yandex.ru>
2021-06-16 15:36:15 +0300

    add digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
   has_many :test_passages
   has_many :tests, through: :test_passages, dependent: :destroy
